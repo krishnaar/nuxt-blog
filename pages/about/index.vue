43c2097e78fb91ea0f87834d523d289f94667fb6
Krishna Al Rasyid <krishnaalrasyid@gmail.com>
2019-12-27 09:52:14 +0700

    creating the Main 'Section'

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about-page">
+        <h1>This is My Absolutely fantastic "About me" Page!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas provident, voluptatem consequuntur aliquid, commodi reprehenderit debitis modi recusandae nesciunt odio qui voluptatibus, quidem alias ullam architecto aut ipsam soluta excepturi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas provident, voluptatem consequuntur aliquid, commodi reprehenderit debitis modi recusandae nesciunt odio qui voluptatibus, quidem alias ullam architecto aut ipsam soluta excepturi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas provident, voluptatem consequuntur aliquid, commodi reprehenderit debitis modi recusandae nesciunt odio qui voluptatibus, quidem alias ullam architecto aut ipsam soluta excepturi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas provident, voluptatem consequuntur aliquid, commodi reprehenderit debitis modi recusandae nesciunt odio qui voluptatibus, quidem alias ullam architecto aut ipsam soluta excepturi.</p>
+    </div>
+</template>
+
+<style scoped>
+    .about-page{
+        text-align: center;
+        padding: 30px;
+    }
+</style>
