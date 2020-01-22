39c1d9470c626a1e045bf6388e514be4eac1a0f5
Krishna Al Rasyid <krishnaalrasyid@gmail.com>
2020-01-22 08:53:00 +0700

    using promise in asyncdata

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="post-page">
-        <PostList />
+        <PostList :posts="loadedPosts"/>
     </div>
 </template>
 
@@ -10,7 +10,35 @@ import PostList from '@/components/Posts/PostList'
 export default {
     components:{
         PostList
-    }    
+    },
+    asyncData(context) {
+        return new Promise((resolve, reject) => {
+            setTimeout (() => {
+                resolve({
+                    loadedPosts: [
+                        { 
+                            id: '1', 
+                            title: "First Post", 
+                            previewText: 'This is our first post!', 
+                            thumbnail: 'http://akb48wup.com/wp-content/uploads/2012/08/shimazaki-haruka-akb48-78.jpg'
+                        },
+                        { 
+                            id: '2', 
+                            title: "Second Post", 
+                            previewText: 'This is our second post!', 
+                            thumbnail: 'http://akb48wup.com/wp-content/uploads/2012/08/shimazaki-haruka-akb48-78.jpg'
+                        }
+                    ]
+                });
+            }, 1500)
+        })
+        .then(data => {
+            return data
+        })
+        .catch(e => {
+            context.error(new Error())
+        })
+    },
 }
 </script>
 
